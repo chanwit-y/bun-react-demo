d187942829f14df4fa58ff5dbb5c4b355aec53ef
chanwit-y <chanwit.yimneam@hotmail.com>
2022-07-14 10:15:07 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import logo from "./logo.svg";
 import * as React from "react";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Button>Test</Button> */}
+      <h1 >Hello world!</h1>
     </div>
   );
 }
